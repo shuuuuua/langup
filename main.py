7dc83520ca88fbf11e1241ba4db7cd2d037c2435
Shu Aiba <shuichiro.aiba@gmail.com>
2023-01-31 23:33:27 +0000

    Jan 31, 2023

--- main.py
+++ main.py
@@ -18,11 +18,34 @@ def shadowing_list():
     blobs = storage_client.list_blobs(shadowing_materials_bucket_name, prefix='voices/')
     mp3_list = [blob.name for blob in blobs if blob.name.endswith('.mp3')]
     
-    return render_template('shadowing_list.html', mp3_list)
+    return render_template('shadowing_list.html', mp3_list = mp3_list)
 
 @app.route("/shadowing/<id>", methods=['GET'])
 def shadowing():
     return render_template('shadowing.html')
+
+@app.route("/flash_speaking")
+def flash_speaking():
+    categories = ['Business1-1', 'Business1-2', 'Business1-3']
+    return render_template('flash_speaking.html', categories=categories)
+
+@app.route("/flash_speaking_play/<string:category>/<int:marked>/<int:idx>")
+def flash_speaking_play(category, marked=1, idx=0):
+    flash_speaking_bucket_name = 'langup-flash-speaking'
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(flash_speaking_bucket_name)
+    blob_contents = bucket.blob(category + '.tsv').download_as_text()
+
+    sentences = []
+    for line in blob_contents.split('\n'):
+        items = line.split('\t')
+        if marked and items[3] is None:
+            continue
+        sentences.append((items[0], items[1]))
+    if idx > len(sentences):
+        return render_template('flash_speaking.html', categories=categories)
+    else:
+        return render_template('flash_speaking_play.html', sentence=sentences[idx], marked=marked, idx=idx)
 
 @app.route("/check", methods=['POST'])
 def cehck():
