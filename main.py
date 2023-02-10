ff3620232db09ff671ad22d1377442894662d93a
Shu Aiba <shuichiro.aiba@gmail.com>
2023-02-10 00:08:53 +0000

    Feb 10

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
+import random
 
 from flask import Flask, render_template, redirect, url_for
+import functions_framework
 from google.cloud import storage
 
 app = Flask(__name__)
 
-@app.route("/")
-def menu():
+random_seed = 0
+flash_speaking_sentences = {}
+
+@functions_framework.http
+#@app.route("/")
+def menu(request):
     return render_template('index.html')
 
 @app.route("/shadowing_list")
@@ -26,24 +32,47 @@ def shadowing():
 
 @app.route("/flash_speaking")
 def flash_speaking():
-    categories = ['Business1-1', 'Business1-2', 'Business1-3']
+    categories = ['Business1-1', 'Business1-2', 'Business1-3-1', 'Advance1-1-1', 'Advance1-1-2']
+    random_seed = int(random.random() * 100)
     return render_template('flash_speaking.html', categories=categories)
 
+@app.route("/flash_speaking_category/<string:category>")
+def flash_speaking_category(category):
+    if category in flash_speaking_sentences.keys():
+        pass
+    else:
+        flash_speaking_bucket_name = 'langup-flash-speaking'
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(flash_speaking_bucket_name)
+        blob_contents = bucket.blob(category + '.tsv').download_as_text()
+
+        sentences = []
+        for line in blob_contents.split('\r\n'):
+            items = line.split('\t')
+            sentences.append(tuple(items))
+        flash_speaking_sentences[category] = sentences
+
+    #print(sentences)
+    return redirect(url_for('flash_speaking_play', category=category, marked=1, idx=1))
+  
+
 @app.route("/flash_speaking_play/<string:category>/<int:marked>/<int:idx>")
-def flash_speaking_play(category, marked=1, idx=0):
+def flash_speaking_play(category, marked=1, idx=1):
     flash_speaking_bucket_name = 'langup-flash-speaking'
     storage_client = storage.Client()
     bucket = storage_client.bucket(flash_speaking_bucket_name)
     blob_contents = bucket.blob(category + '.tsv').download_as_text()
 
-    sentences = []
-    for line in blob_contents.split('\n'):
-        items = line.split('\t')
-        if marked and items[3] is None:
-            continue
-        sentences.append((items[0], items[1]))
-    if idx < len(sentences):
-        return render_template('flash_speaking_play.html', sentence=sentences[idx], marked=marked, idx=idx)
+    if category not in flash_speaking_sentences.keys():
+        return redirect(url_for('flash_speaking_category', category=category))
+
+    sentences = [sentence for sentence in flash_speaking_sentences[category] if marked and sentence[4] == '1']
+    #print(sentences)
+
+    random.shuffle(sentences)
+    total = len(sentences)
+    if idx - 1 < total:
+        return render_template('flash_speaking_play.html', sentence=sentences[idx-1], marked=marked, idx=idx, total=total)
     else:
         return redirect(url_for('flash_speaking'))
 
